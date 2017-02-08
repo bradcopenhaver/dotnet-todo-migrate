564b70acebb8d68fdd35e402ab4a12081223f513
Brad Copenhaver and Ryan Peterson <brad+ryan@>
2017-02-08 08:50:09 -0800

    add migration files

--- src/ToDoList/Models/ToDoListContext.cs
+++ src/ToDoList/Models/ToDoListContext.cs
@@ -5,11 +5,26 @@ namespace ToDoList.Models
 {
     public class ToDoListContext : DbContext
     {
+        public ToDoListContext()
+        {
+
+        }
         public virtual DbSet<Category> Categories { get; set; }
         public virtual DbSet<Item> Items { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
             options.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=ToDoList;integrated security=True;");
+        }
+
+        public ToDoListContext(DbContextOptions<ToDoListContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
         }
     }
 }
